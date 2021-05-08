3d5f96cbeaff8b019de735e7fe2b947af061f5ce
leish0n6 <saprykinsemen@gmail.com>
2021-05-08 19:22:31 +0300

    stepik

---
+++ stepik_2.py
@@ -0,0 +1,15 @@
+# name = input()
+#
+# print('Привет', name)
+# print('Привет, Гвидо')
+
+# print('a', 'b', 'c', end='@')
+# print('d', 'e', 'f', end='@@')
+
+# name = input()
+# print('Привет,', name, end='!')
+
+# first = int(input())
+# sec = int(input())
+#
+# print((3 * ((first + sec) ** 3)) + (275 * (sec ** 2)) - (127 * first) - 41)
